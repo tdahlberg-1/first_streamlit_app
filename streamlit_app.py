2245decd5ed1ed2f4743de583a4c4e7f7006ee16
Thomas Dahlberg <97476537+tdahlbergsnow@users.noreply.github.com>
2022-11-09 21:14:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #= my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list") #remove if above is used
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list") #remove if above is used
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("The Fruit List contains:")
