97748e5e8c964033ee02f17fb610062fcda22eaf
Thomas Dahlberg <97476537+tdahlbergsnow@users.noreply.github.com>
2022-11-10 12:31:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,10 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-else:
+        try:
+        fruit_choice = streamlit.text_input('What fruit would you like information about?')
+        if not fruit_choice:
+    else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
